0dc748bc87756b610da4a6f80eba9095ffe06812
Subhan De <desubhan123@gmail.com>
2025-01-18 07:52:11 +0530

    Add rod cutting problem solution with recursive, memoization, tabulation, and space optimized approaches

---
+++ dynamic-programming/dp-on-subsequence/rod_cutting_problem.py
@@ -0,0 +1,60 @@
+"""
+Rod cutting problem
+----------------------
+Given a rod of length N inches and an array price[] where price[i] denotes the value of a piece of rod of length i inches (1-based indexing). Determine the maximum value obtainable by cutting up the rod and selling the pieces. Make any number of cuts, or none at all, and sell the resulting pieces.
+
+
+Examples:
+Input: price = [1, 6, 8, 9, 10, 19, 7, 20], N = 8
+Output: 25
+
+Explanation: Cut the rod into lengths of 2 and 6 for a total price of 6 + 19= 25.
+
+-------------------------------------------------------------
+Input: price = [1, 5, 8, 9], N = 4
+Output: 10
+
+Explanation: Cut the rod into lengths of 2 and 2 for a total price of 5 + 5 = 10.
+    
+    
+Constraints:
+1 ≤ N ≤ 1000
+1 ≤ price[i] ≤ 10^5
+
+"""
+
+class Solution:
+
+    ################################
+    # Recursive Approach
+    ################################
+
+    def rodCuttingKnapsackRecursiveHelper(self, coins, index, remainingAmount):
+        pass
+
+    def rodCuttingKnapsackRecursive(self, coins, numCoins, totalAmount):
+        pass
+
+    ################################
+    # Memoization Approach
+    ################################
+
+    def rodCuttingMemoizationHelper(self, coins, index, remainingAmount, memo):
+        pass
+
+    def rodCuttingMemoization(self, coins, numCoins, totalAmount):
+        pass
+
+    ################################
+    # Tabulation Approach
+    ################################
+
+    def rodCuttingTabulation(self, coins, numCoins, totalAmount):
+        pass
+
+    ################################
+    # Tabulation Space Optimized Approach
+    ################################
+
+    def rodCuttingTabulationSpaceOptimized(self, coins, numCoins, totalAmount):
+        pass
