353672c9366b190243b0ce3d97b9e986261b66e9
Subhan De <desubhan123@gmail.com>
2025-01-28 20:01:07 +0530

    rod cutting added

--- dynamic-programming/dp-on-subsequence/rod_cutting_problem.py
+++ dynamic-programming/dp-on-subsequence/rod_cutting_problem.py
@@ -15,8 +15,8 @@ Input: price = [1, 5, 8, 9], N = 4
 Output: 10
 
 Explanation: Cut the rod into lengths of 2 and 2 for a total price of 5 + 5 = 10.
-    
-    
+
+
 Constraints:
 1 ≤ N ≤ 1000
 1 ≤ price[i] ≤ 10^5
@@ -29,32 +29,116 @@ class Solution:
     # Recursive Approach
     ################################
 
-    def rodCuttingKnapsackRecursiveHelper(self, coins, index, remainingAmount):
-        pass
+    def rodCuttingRecursiveHelper(self, price, ind, N):
+        if ind == 0:
+            return price[0] * N
 
-    def rodCuttingKnapsackRecursive(self, coins, numCoins, totalAmount):
-        pass
+        notTaken = self.rodCuttingRecursiveHelper(price, ind - 1, N)
+
+        rodLength = ind + 1
+        taken = float("-inf")
+
+        if rodLength <= N:
+            taken = price[ind] + self.rodCuttingRecursiveHelper(price, ind, N - rodLength)
+
+        return max(taken, notTaken)
+
+    def rodCuttingRecursive(self, price, n):
+        return self.rodCuttingRecursiveHelper(price, n-1, n)
 
     ################################
     # Memoization Approach
     ################################
 
-    def rodCuttingMemoizationHelper(self, coins, index, remainingAmount, memo):
-        pass
+    def rodCuttingMemoizationHelper(self, price, ind, N, memo):
+        if ind == 0:
+            return price[0] * N
 
-    def rodCuttingMemoization(self, coins, numCoins, totalAmount):
-        pass
+        if memo[ind][N] != 0:
+            return memo[ind][N]
+
+        notTaken = self.rodCuttingRecursiveHelper(price, ind - 1, N)
+
+        rodLength = ind + 1
+        taken = float("-inf")
+
+        if rodLength <= N:
+            taken = price[ind] + self.rodCuttingRecursiveHelper(price, ind, N - rodLength)
+
+        return max(taken, notTaken)
+
+    def rodCuttingMemoization(self, price, n):
+        memo = [[0] * (n + 1) for _ in range(n + 1)]
+
+        return self.rodCuttingMemoizationHelper(price, n-1, n, memo)
 
     ################################
     # Tabulation Approach
     ################################
 
-    def rodCuttingTabulation(self, coins, numCoins, totalAmount):
-        pass
+    def rodCuttingTabulation(self, price, n):
+        memo = [[0] * (n + 1) for _ in range(n)]
+
+        for length in range(n+1):
+            memo[0][length] = price[0] * length
+
+        for ind in range(1, n):
+            for length in range(1, n + 1):
+                notTaken = memo[ind - 1][length]
+                rodLength = ind + 1
+                taken = float("-inf")
+                if rodLength <= length:
+                    taken = price[ind] + memo[ind][length - rodLength]
+
+                memo[ind][length] = max(taken, notTaken)
+
+        return memo[n-1][n]
 
     ################################
     # Tabulation Space Optimized Approach
     ################################
 
-    def rodCuttingTabulationSpaceOptimized(self, coins, numCoins, totalAmount):
-        pass
+    def rodCuttingTabulationSpaceOptimized(self, price, n):
+        prev = [0] * (n + 1)
+        cur = [0] * (n + 1)
+
+        for length in range(n+1):
+            prev[length] = price[0] * length
+
+        for ind in range(1, n):
+            for length in range(1, n + 1):
+                notTaken = prev[length]
+                rodLength = ind + 1
+                taken = float("-inf")
+                if rodLength <= length:
+                    taken = price[ind] + cur[length - rodLength]
+
+                cur[length] = max(taken, notTaken)
+            prev = cur[:]
+        return prev[n]
+
+
+if __name__ == "__main__":
+    # Create an instance of Solution class
+    sol = Solution()
+
+    # Test cases
+    test_cases = [
+        {"price": [1, 6, 8, 9, 10, 19, 7, 20], "N": 8, "expected": 25},
+        {"price": [1, 5, 8, 9], "N": 4, "expected": 8},
+        {"price": [5, 5, 8, 9, 10, 17, 17, 20], "N": 8, "expected": 22},
+    ]
+
+    for test in test_cases:
+        price = test["price"]
+        N = test["N"]
+
+        print("="*50)
+
+        print("The maximum value is (Recursive):", sol.rodCuttingRecursive(price, N))
+
+        print("The maximum value is (Memoization):", sol.rodCuttingMemoization(price, N))
+
+        print("The maximum value is (Tabulation):", sol.rodCuttingTabulation(price, N))
+
+        print("The maximum value is (Tabulation Space Optimized):", sol.rodCuttingTabulationSpaceOptimized(price, N))
